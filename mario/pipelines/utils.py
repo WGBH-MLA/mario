b2df87b5f8ed89bed1c3fc57b401c4cbdd7d7670
Harpo Harbert <ryan_harbert@wgbh.org>
2023-10-13 12:02:52 -0700

    Migrates to mmif_location

--- mario/pipelines/utils.py
+++ mario/pipelines/utils.py
@@ -1,7 +1,7 @@
 class PipelineUtils:
     """Utility functions for CLAMS Pipeline Runner"""
 
-    def get_asset_id(self):
+    def get_asset_id(self) -> None:
         from chowda.db import engine
         from chowda.models import Batch, MediaFile, MetaflowRun
         from metaflow import current
@@ -38,18 +38,7 @@ class PipelineUtils:
             self.type = asset.type.value.lower()
             self.filename = join('/m', self.asset_name)
 
-    def get_mmif_from_database(self):
-        from chowda.db import engine
-        from chowda.models import MediaFile
-        from sqlmodel import Session, select
-
-        with Session(engine) as db:
-            media_file = db.exec(
-                select(MediaFile).where(MediaFile.guid == self.guid)
-            ).one()
-            return media_file.mmif_json
-
-    def create_new_mmif(self):
+    def create_new_mmif(self) -> dict:
         from requests import post
 
         self.input_mmif = post(
@@ -57,7 +46,7 @@ class PipelineUtils:
             json={'files': [f'{self.type}:' + self.filename]},
         ).json()
 
-    def download_media_file(self):
+    def download_media_file(self) -> None:
         """Download the media file"""
         from urllib.request import urlretrieve
 
@@ -70,7 +59,7 @@ class PipelineUtils:
         print('Downloading file')
         urlretrieve(url, self.filename)
 
-    def app(self, app, mmif):
+    def app(self, app: str, mmif: dict) -> dict:
         """Run the mmif through a CLAMS app"""
         from requests import post
 
@@ -83,27 +72,49 @@ class PipelineUtils:
             )
         return response.json()
 
-    def update_database(self):
+    def update_database(self, s3_path: str) -> None:
+        """Update the database with the output mmif"""
         from chowda.db import engine
-        from chowda.models import MediaFile
+        from chowda.models import MMIF, Batch, MediaFile, MetaflowRun
+        from metaflow import current
         from sqlmodel import Session, select
 
         with Session(engine) as db:
             media_file = db.exec(
                 select(MediaFile).where(MediaFile.guid == self.guid)
             ).one()
-            media_file.mmif_json = self.output_mmif
-            db.add(media_file)
+            batch = db.get(Batch, self.batch_id)
+            metaflow_run = db.get(MetaflowRun, current.run_id)
+            mmif = MMIF(
+                media_file=media_file,
+                metaflow_run=metaflow_run,
+                batch_output=batch,
+                mmif_location=s3_path,
+            )
+            db.add(mmif)
             db.commit()
 
-    def upload_s3(self):
+    def download_mmif(self, s3_path: str) -> dict:
+        """Download an mmif from S3"""
+        from json import loads
+
+        from boto3 import client
+
+        bucket = self.bucket if self.bucket != 'null' else 'clams-mmif'
+        client = client('s3')
+        response = client.get_object(Bucket=bucket, Key=s3_path)
+        body = response['Body'].read().decode('utf-8')
+        return loads(body)
+
+    def upload_mmif(self) -> None:
+        """Upload the output mmif to S3"""
         from json import dumps
         from os.path import join
 
         from boto3 import client
 
         mmif_filename = join('/m', self.guid + '.mmif')
-        s3_path = f'{self.guid}/app-barsdetection/{self.guid}.mmif'
+        s3_path = f'{self.guid}/{self.batch_id}/{self.guid}.mmif'
 
         with open(mmif_filename, 'w') as f:
             f.write(dumps(self.output_mmif))
@@ -118,7 +129,7 @@ class PipelineUtils:
             s3_path,
         )
 
-    def cleanup(self):
+    def cleanup(self) -> None:
         """delete media file and transcripts"""
         from glob import glob
         from os import remove
